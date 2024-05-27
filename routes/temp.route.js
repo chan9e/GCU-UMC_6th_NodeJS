79ef5cf4860888b5dac4366eb3fa9b5ce7170bd5
chan9e <fogusrkgus@naver.com>
2024-05-28 08:15:58 +0900

    commit mission8

--- routes/temp.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { tempTest } from '../controllers/temp.controller.js';
-import { tempException } from '../controllers/temp.controller.js';
-
-export const tempRouter = express.Router();
-
-tempRouter.get('/test', tempTest);
-
-tempRouter.get('/exception/:flag', tempException);
